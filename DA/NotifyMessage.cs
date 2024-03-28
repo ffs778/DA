ceb4eb8835fa98c59d4a8d44f8665100a417e848
王海平 <2530611886@qq.com>
2024-03-28 12:00:40 +0800

    添加NotifyIcon通知类

---
+++ DA/NotifyMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DA
+{
+    public class NotifyMessage
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg"></param>
+        public static void ShowSuccess(string msg)
+        {
+            NotifyIcon notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = Properties.Resources.cranesIcon;
+            notifyIcon.Visible = true;
+            notifyIcon.ShowBalloonTip(2000, "Success", msg, ToolTipIcon.Info);
+        }
+    }
+}
--- DA/Properties/Resources.Designer.cs
+++ DA/Properties/Resources.Designer.cs
@@ -81,6 +81,16 @@ namespace DA.Properties {
         }
         
         /// <summary>
+        ///   查找类似于 (图标) 的 System.Drawing.Icon 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Icon cranesIcon {
+            get {
+                object obj = ResourceManager.GetObject("cranesIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
         internal static System.Drawing.Bitmap form {
